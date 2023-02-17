68eb8aaae9fa642fca973d786f4af140efc9072f
AnishKing2004 <67561021+AnishKing2004@users.noreply.github.com>
2023-02-16 18:31:46 -0800

    Big pp gang yezzir

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -141,13 +141,13 @@ public class CakeView extends SurfaceView {
 
 
 
-
         //Draw two candles equidistant from each other
         drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
         drawCandle(canvas, cakeLeft + 2*cakeWidth/3 - candleWidth/2, cakeTop);
 
         textPaint.setTextSize(100);
         canvas.drawText("Coordinates" + cakeModel.x + " ," + cakeModel.y + " ", 100,1100,textPaint );
+        //big pp gang
 
 
 
